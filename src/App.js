6338648bd55605d7a193a65b617cbffc7fce84a6
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-13 20:34:10 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({});
  const provider = new GoogleAuthProvider();
  const handleGoogleSingIn = () =>{
   signInWithPopup(auth, provider)
   .then(result =>{
     const user = result.user
     console.log(user)
+    setUser(user)
   })
   .catch(error =>{
     console.error(error)
+
   })
+ }
+ const handleSingOut = () =>{
+    signOut(auth)
+    .then( () =>{
+      setUser({})
+    } )
+    .catch(() => {
+      setUser({})
+    })
  }
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSingIn}>Google SingIn</button>
+     { user.email ?
+     <button onClick={handleSingOut}>Sing Out</button>
+     :
+       <button onClick={handleGoogleSingIn}>Google SingIn</button>}
+     { user.email && <div>
+       <h2>User Name: {user.displayName}</h2> 
+       <p> User Email: {user.email}</p>
+       <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
