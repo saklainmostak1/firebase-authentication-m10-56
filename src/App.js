bf935aee38adaf7154517275e45b6f9a2a993e80
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-13 06:57:44 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
 import app from './firebase/firebase.init';
 
 const auth = getAuth(app)
 
 function App() {
-  const provider = GoogleAuthProvider();
+ const provider = new GoogleAuthProvider();
+ const handleGoogleSingIn = () =>{
+  signInWithPopup(auth, provider)
+  .then(result =>{
+    const user = result.user
+    console.log(user)
+  })
+  .catch(error =>{
+    console.error(error)
+  })
+ }
+
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSingIn}>Google SingIn</button>
     </div>
   );
 }
